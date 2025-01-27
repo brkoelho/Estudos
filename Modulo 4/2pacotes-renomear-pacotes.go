902e38dc6a3152a93550b1d4b5f6b8306110f94d
Bruno Rodrigues Coelho <brkoelho@hotmail.com>
2025-01-27 10:28:04 -0300

    Add files via upload

---
+++ Modulo 4/2pacotes-renomear-pacotes.go
@@ -0,0 +1,14 @@
+// Início
+package main
+
+//Importação de pacotes
+import (
+	p "fmt"     //É o padrão de println, essa questão de por p antes do pacote é o jeito de renomear
+	s "strings" //É outra função...
+)
+
+// Igual steph disse é o par do package main...
+func main() {
+	p.Println("Cheguei Brasil!")
+	p.Println(s.Split("XD", ""))
+}
